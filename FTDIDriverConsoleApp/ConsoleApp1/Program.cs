6e5bd2aa7ebf360e6cceed86d279a14184d880f5
shak48 <61759664+shak48@users.noreply.github.com>
2023-09-15 21:52:12 -0400

    Update Program.cs
    Try fo ftdi.

--- FTDIDriverConsoleApp/ConsoleApp1/Program.cs
+++ FTDIDriverConsoleApp/ConsoleApp1/Program.cs
@@ -65,3 +65,74 @@ class Program
         }
     }
 }
+
+
+/*
+To try.
+
+To start I2C communication with a C232HM MPSSE FTDI device as a master device using .NET Framework in a console app, you can use the FTD2XX library provided by FTDI. Here's a basic example of how to do this:
+
+Please note that you need to install the FTD2XX library and add a reference to it in your .NET Framework console application.
+
+```csharp
+using System;
+using FTD2XX; // Make sure to add a reference to the FTD2XX library.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FTDI ftdi = new FTDI();
+
+        // Open the FTDI device by its description or serial number
+        FTDI.FT_DEVICE_INFO_NODE[] deviceList = new FTDI.FT_DEVICE_INFO_NODE[1];
+        ftdi.GetDeviceList(deviceList);
+        int deviceCount = deviceList.Length;
+
+        if (deviceCount == 0)
+        {
+            Console.WriteLine("No FTDI devices found.");
+            return;
+        }
+
+        // You can also use device serial number or other criteria to select the device.
+        // For example: ftdi.OpenBySerialNumber("YourSerialNumber");
+
+        FTDI.FT_STATUS status = ftdi.OpenByIndex(0); // Open the first FTDI device
+        if (status != FTDI.FT_STATUS.FT_OK)
+        {
+            Console.WriteLine("Failed to open FTDI device. Error: " + status.ToString());
+            return;
+        }
+
+        // Configure the FTDI device for MPSSE mode
+        status = ftdi.SetBitMode(0x0, FTDI.FT_BIT_MODES.FT_BIT_MODE_RESET);
+        if (status != FTDI.FT_STATUS.FT_OK)
+        {
+            Console.WriteLine("Failed to reset FTDI device. Error: " + status.ToString());
+            return;
+        }
+
+        status = ftdi.SetBitMode(0x0, FTDI.FT_BIT_MODES.FT_BIT_MODE_MPSSE);
+        if (status != FTDI.FT_STATUS.FT_OK)
+        {
+            Console.WriteLine("Failed to set MPSSE mode. Error: " + status.ToString());
+            return;
+        }
+
+        // Configure I2C communication parameters
+        ftdi.SetBaudRate(100000); // Set the I2C clock rate (100 kHz in this example)
+
+        // Start I2C communication here. You can use the MPSSE commands to send I2C data.
+
+        // Don't forget to close the FTDI device when you're done.
+        ftdi.Close();
+
+        Console.WriteLine("I2C communication completed.");
+    }
+}
+```
+
+This code opens the FTDI device, configures it for MPSSE mode, sets the I2C clock rate, and provides a starting point for your I2C communication. You will need to use the MPSSE commands to send and receive I2C data according to your specific requirements. Make sure to consult the FTDI documentation for the MPSSE commands and the capabilities of the C232HM MPSSE device.
+
+*/
